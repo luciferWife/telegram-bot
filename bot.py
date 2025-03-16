d7cc2e0aec48f7e75b2938cec30346e1a840be79
luciferWife <khasanovajasmin2503@gmail.com>
2025-03-16 16:58:10 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-git add .
-git commit -m "Перенесла токен в переменные окружения"
-git push origin main
 
+# Берём токен из переменной окружения (Railway)
 TOKEN = os.getenv("BOT_TOKEN")
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+# Главное меню
 main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 main_menu.add(KeyboardButton("Регистрация"))
 
